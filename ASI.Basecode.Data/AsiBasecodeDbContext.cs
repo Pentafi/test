e6cfd10e12b957d3d23458e1f32a0fad93f5b5b5
Justinxd <justin.gabison.20@usjr.edu.ph>
2023-11-03 20:09:25 +0800

    Added Review but not fixed

--- ASI.Basecode.Data/AsiBasecodeDbContext.cs
+++ ASI.Basecode.Data/AsiBasecodeDbContext.cs
@@ -34,6 +34,7 @@ namespace ASI.Basecode.Data
         public virtual DbSet<Book> Books { get; set; }
         public virtual DbSet<Author> Authors { get; set; }
         public virtual DbSet<Genre> Genres { get; set; }
+        public virtual DbSet<Review> Review { get; set; }
 
 
         /*protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ ASI.Basecode.Data/Interfaces/IReviewRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ASI.Basecode.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Data.Interfaces
+{
+    class IReviewRepository
+    {
+        IQueryable<Review> GetAllReviews();
+        Author GetReviewsById(int id);
+        void AddReview(Review review);
+        void UpdateReview(Review review);
+        void DeleteReview(int id);
+        bool ReviewExists(int id);
+    }
+}
