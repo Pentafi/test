3c5bf3b113e499a8d62b45ef3b3a62e9a9ea223c
Julius Palalay <julius.palalay@usjr.edu.ph>
2023-11-07 00:11:43 +0800

    Home Page, Footer and Header Created

--- ASI.Basecode.WebApp/Controllers/BookController.cs
+++ ASI.Basecode.WebApp/Controllers/BookController.cs
@@ -1,12 +1,104 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PogoAdmin.Services;
+using Services.ServiceModels;
 
 namespace ASI.Basecode.WebApp.Controllers
 {
     public class BookController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly IBookService _bookService;
+        private readonly IAuthorService _authorService;
+
+        public BookController(IMapper mapper, IBookService bookService, IAuthorService authorService)
+        {
+            _mapper = mapper;
+            _bookService = bookService;
+            _authorService = authorService;
+        }
+
         public IActionResult Index()
         {
+            var bookViewModels = _bookService.GetAllBooks();
+            return View(bookViewModels);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var viewModel = _bookService.GetBookById(id);
+            if (viewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var authors = _authorService.GetAllAuthors()
+                                   .Select(a => new
+                                   {
+                                       ID = a.authorID,
+                                       FullName = a.firstName + " " + a.lastName
+                                   })
+                                   .ToList();
+            ViewBag.AuthorList = new SelectList(authors, "Id", "FullName");
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BookViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookService.AddBook(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var viewModel = _bookService.GetBookById(id);
+            var authors = _authorService.GetAllAuthors()
+                                   .ToList()
+                                   .Select(a => new
+                                   {
+                                       ID = a.authorID,
+                                       FullName = a.firstName + " " + a.lastName
+                                   })
+                                   .ToList();
+            if (viewModel == null)
+            {
+                return NotFound();
+            }
+            ViewBag.AuthorList = new SelectList(authors, "Id", "FullName");
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(BookViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookService.UpdateBook(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _bookService.DeleteBook(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
