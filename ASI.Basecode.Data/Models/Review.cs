cc5fe718af5ebc0a8a0ff2385daaa4c5cc9d2c63
Justinxd <justin.gabison.20@usjr.edu.ph>
2023-11-10 22:07:59 +0800

    Added Review on Data folder

--- ASI.Basecode.Data/Models/Review.cs
+++ ASI.Basecode.Data/Models/Review.cs
@@ -1,19 +1,20 @@
-﻿/*using System;
+﻿using ASI.Basecode.Data.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASI.Basecode.Data.Models
 {
     public partial class Review
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string content { get; set; }
         public int Rating { get; set; }
         public string BookId { get; set; }
         public DateTime dateReviewed { get; set; }
+        public virtual Book Book { get; set; }
     }
-} */
+}
+
