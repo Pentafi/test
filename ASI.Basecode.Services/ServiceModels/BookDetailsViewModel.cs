7b752e2b0b26afa308f6b8442cf60643865bc771
ReneirGwapo <reneir.panares@usjr.edu.ph>
2023-12-01 20:50:38 +0800

    update on services and controller

---
+++ ASI.Basecode.Services/ServiceModels/BookDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ASI.Basecode.Services.Models;
+using Services.ServiceModels;
+
+namespace ASI.Basecode.Services.Models
+{
+    public class BookDetailsViewModel
+    {
+        public BookViewModel Book { get; set; }
+        public List<BookViewModel> AllBooks { get; set; } 
+        public BookViewModel[] RelatedBooks { get; set; }
+
+
+    }
+}
---
+++ ASI.Basecode.WebApp/Controllers/AuthorController.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PogoAdmin.Services;
+using Services.Models;
+using System;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IAuthorService _authorService;
+        private readonly ILogger<AuthorController> _logger; 
+
+        public AuthorController(IMapper mapper, IAuthorService authorService, ILogger<AuthorController> logger) 
+        {
+            _mapper = mapper;
+            _authorService = authorService;
+            _logger = logger; 
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            var viewModel = _authorService.GetAuthorById(id);
+            if (viewModel == null)
+            {
+                _logger.LogWarning("Author with ID {AuthorId} not found.", id); 
+                return NotFound();
+            }
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AuthorViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _authorService.AddAuthor(model);
+                _logger.LogInformation("Author created: {AuthorName} {LastName}", model.FirstName, model.LastName); 
+                return RedirectToAction(nameof(Index));
+            }
+            _logger.LogWarning("Invalid model state for creating author."); 
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var viewModel = _authorService.GetAuthorById(id);
+            if (viewModel == null)
+            {
+                _logger.LogWarning("Edit attempted for non-existent author with ID {AuthorId}.", id); 
+                return NotFound();
+            }
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(AuthorViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _authorService.UpdateAuthor(model);
+                _logger.LogInformation("Author updated: {AuthorId}", model.Id); 
+                return RedirectToAction(nameof(Index));
+            }
+            _logger.LogWarning("Invalid model state for editing author with ID {AuthorId}.", model.Id); 
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _authorService.DeleteAuthor(id);
+            _logger.LogInformation("Author deleted: {AuthorId}", id); 
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
