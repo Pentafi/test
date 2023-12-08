c80ceb109c646aac68963d92dd477240fed3e861
Mico <earl.filosopo@usjr.edu.ph>
2023-12-08 16:58:19 +0800

    added superAdmin functionality

---
+++ ASI.Basecode.WebApp/Models/RoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ASI.Basecode.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace ASI.Basecode.WebApp.Models
+{
+    public class RoleViewModel
+    {
+        public User User { get; set; }
+        public List<string> UserRoles { get; set; }
+    }
+}
---
+++ ASI.Basecode.WebApp/Models/UserViewRoleModel.cs
@@ -0,0 +1,11 @@
+﻿using ASI.Basecode.Services.ServiceModels;
+using System.Collections.Generic;
+
+namespace ASI.Basecode.WebApp.Models
+{
+    public class UserViewRoleModel
+    {
+        public UserViewModel ViewModel { get; set; }
+        public List<RoleViewModel> IdentityUsers { get; set; }
+    }
+}
